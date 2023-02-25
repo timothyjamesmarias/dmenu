ce902cac6bc47fb5be828631dd35fb96e1bdc6a3
Timothy Marias <timothyjamesmarias@gmail.com>
2023-02-25 10:29:54 -0800

    update

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"ubuntumono:size=14",
+	"iosevka:size=14",
 	"NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#16171e" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
